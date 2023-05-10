df0247ae39d01623a78407d0c08d458580b15958
firepro <bengamble7@gmail.com>
2023-05-10 21:35:46 +0100

    a few more fixes, the data needs a bit of an adjustment, but so far it's good

--- makeData.py
+++ makeData.py
@@ -1,8 +1,15 @@
+import datetime
 import json
 import random
 from pprint import pprint
 
+import aiokafka
+from aiokafka import AIOKafkaProducer
+import asyncio
+
+from aiokafka.helpers import create_ssl_context
 from pydantic import BaseModel
+from snowflake.snowflake import SnowflakeGenerator
 
 from celebrationData import make_my_event_scores
 from citydata import make_my_cities
@@ -11,6 +18,14 @@ from product_values import calculate_day_score
 
 import numpy as np
 from noise import snoise2
+
+context = create_ssl_context(
+    cafile="demo_keys/ca.pem",
+    certfile="demo_keys/service.cert",
+    keyfile="demo_keys/service.key",
+)
+
+TOPIC_NAME = "input-events"
 
 
 def generate_simplex_noise(period, num_values, amplitude=1, seed=0, octaves=1, persistence=0.5):
@@ -32,11 +47,20 @@ def generate_simplex_noise(period, num_values, amplitude=1, seed=0, octaves=1, p
 
 
 class Purchase(BaseModel):
+    id: int
     city: str
-    day: int
     item: str
     price: float
     influencer: str = None
+
+
+class Basket(BaseModel):
+    id: int
+    city: str
+    purchase_day: int
+    total_purchases: int
+    purchases: list[Purchase]
+    pass
 
 
 def get_random_item_from_dict(my_dict):
@@ -49,98 +73,145 @@ def seeded_random(seed):
         yield random.random()
 
 
-random_gen = seeded_random(42)
-period = 365
-num_values = 365
-amplitude = 1
-seed = 42
-octaves = 3
-persistence = 0.5
+async def my_loop(my_producer):
+    topic_name = "basket-input"
+    producer = my_producer
+    random_gen = seeded_random(42)
+    period = 365
+    num_values = 365
+    amplitude = 1
+    seed = 42
+    octaves = 3
+    persistence = 0.5
+    id_gen = SnowflakeGenerator(42)
+    basket_id = SnowflakeGenerator(420)
+    simplex_noise_values = generate_simplex_noise(period, num_values, amplitude, seed, octaves, persistence)
 
-simplex_noise_values = generate_simplex_noise(period, num_values, amplitude, seed, octaves, persistence)
+    my_cities = make_my_cities()
 
-my_cities = make_my_cities()
+    my_events = make_my_event_scores()
 
-my_events = make_my_event_scores()
+    my_groceries = GroceryList()
 
-my_groceries = GroceryList()
+    purchases_per_day = 10009
+    newyear_purchases = 0
+    easter_purchases = 0
+    thanksgiving_purchases = 0
+    xmas_purchases = 0
+    halloween_purchases = 0
 
-purchases_per_day = 100
-newyear_purchases = 0
-easter_purchases = 0
-thanksgiving_purchases = 0
-xmas_purchases = 0
-halloween_purchases = 0
+    total_purchases = 0
 
-total_purchases = 0
+    start_date = datetime.datetime(2020, 1, 1) + datetime.timedelta(hours=3.4)
 
+    for city in my_cities.values():
+        for day in range(365):
 
-
-for city in my_cities.values():
-    for day in range(365):
-
-        my_score = calculate_day_score(day, city, my_events)
-        weighted_purchases = my_score.spend_weight * (simplex_noise_values[day] + 1) * purchases_per_day
-        daily_purchases = []
-        if my_score.valentines_weight > 0.3:
-            print(f"Valentines day in {city.name} on day {day}")
-            pass
-
-        while weighted_purchases > 0:
-            weighted_purchases -= 1
-            purchase = Purchase(city="city", day=1, item="item", price=10)
-            purchase.city = city.name
-            purchase.day = day
-            purchase.item, purchase.price = get_random_item_from_dict(my_groceries.general)
-
-            daily_purchases.append(purchase.json())
-            my_rand = random_gen.__next__()
-            if random_gen.__next__() < my_score.xmas_weight:
-                next_item, next_price = get_random_item_from_dict(my_groceries.xmas)
-                xmas_purchase = Purchase(city = city.name, day = day, item = next_item, price = next_price, influencer = "xmas")
-                daily_purchases.append(xmas_purchase.json())
-                weighted_purchases -= 1
-                xmas_purchases += 1
-
-            if random_gen.__next__() < my_score.thanksgiving_weight:
-                next_item, next_price = get_random_item_from_dict(my_groceries.thanksgiving)
-                thanksgiving_purchase = Purchase(city = city.name, day = day, item = next_item, price = next_price, influencer = "thanksgiving")
-                daily_purchases.append(thanksgiving_purchase.json())
-                weighted_purchases -= 1
-                thanksgiving_purchases += 1
-
-            if random_gen.__next__() < my_score.easter_weight:
-                next_item, next_price = get_random_item_from_dict(my_groceries.easter)
-                easter_purchase =Purchase(city = city.name, day = day, item = next_item, price = next_price, influencer = "easter")
-                daily_purchases.append(easter_purchase.json())
-                weighted_purchases -= 1
-                easter_purchases += 1
-
-            # if my_score.valentines_weight > 0.3:
-            #     print(f"Valentines day in {city.name} on day {day}")
-            #     print(f"Valentines weight is {my_score.valentines_weight}, random is {random_gen.__next__()}"                )
-
-            if random_gen.__next__() < my_score.valentines_weight:
-                next_item, next_price = get_random_item_from_dict(my_groceries.valentines)
-                valentines_purchase = Purchase(city = city.name, day = day, item = next_item, price = next_price)
-
-                daily_purchases.append(valentines_purchase.json())
-                weighted_purchases -= 1
-
-            if random_gen.__next__() < my_score.new_year_weight:
-                next_item, next_price = get_random_item_from_dict(my_groceries.new_year)
-                new_year_purchase = Purchase(city = city.name, day = day, item = next_item, price = next_price, influencer = "new_year")
-                daily_purchases.append(new_year_purchase.json())
-                weighted_purchases -= 1
-                newyear_purchases += 1
-
-        # print(daily_purchases)
-        total_purchases += len(daily_purchases)
-        json_purchases = json.dumps(daily_purchases)
+            my_score = calculate_day_score(day, city, my_events)
+            holiday_weighting = ((1 + my_score.xmas_weight) *
+                                 (1 + my_score.thanksgiving_weight) *
+                                 (1 + my_score.easter_weight) *
+                                 (1 + my_score.halloween_weight) *
+                                 (1 + my_score.newyear_weight))
+            weighted_purchases = my_score.spend_weight * (simplex_noise_values[day] + 1) * purchases_per_day * holiday_weighting
+            daily_purchases = []
+            current_day = start_date + datetime.timedelta(days=day)
 
 
+            while weighted_purchases > 0:
+                basket_size = max(round(random.gauss(25 * holiday_weighting, 12)), 2) + 2
+                my_basket = Basket(id=next(basket_id), city=city.name, purchase_day=current_day.timestamp(),
+                                   purchases=[], total_purchases=basket_size)
+
+                for basket_item in range(basket_size):
+                    if weighted_purchases <= 0:
+                        break
+                    weighted_purchases -= 1
+                    purchase = Purchase(id=next(id_gen), city="city", item="item", price=10)
+                    purchase.city = city.name
+                    purchase.item, purchase.price = get_random_item_from_dict(my_groceries.general)
+
+                    daily_purchases.append(purchase.json())
+                    my_basket.purchases.append(purchase)
+                    if random_gen.__next__() < my_score.xmas_weight:
+                        next_item, next_price = get_random_item_from_dict(my_groceries.xmas)
+                        xmas_purchase = Purchase(id=next(id_gen), city=city.name,
+                                                 item=next_item,
+                                                 price=next_price,
+                                                 influencer="xmas")
+                        daily_purchases.append(xmas_purchase.json())
+                        my_basket.purchases.append(xmas_purchase)
+
+                        weighted_purchases -= 1
+                        xmas_purchases += 1
+
+                    if random_gen.__next__() < my_score.thanksgiving_weight:
+                        next_item, next_price = get_random_item_from_dict(my_groceries.thanksgiving)
+                        thanksgiving_purchase = Purchase(id=next(id_gen), city=city.name,
+                                                         item=next_item,
+                                                         price=next_price,
+                                                         influencer="thanksgiving")
+                        daily_purchases.append(thanksgiving_purchase.json())
+                        my_basket.purchases.append(thanksgiving_purchase)
+                        weighted_purchases -= 1
+                        thanksgiving_purchases += 1
+
+                    if random_gen.__next__() < my_score.easter_weight:
+                        next_item, next_price = get_random_item_from_dict(my_groceries.easter)
+                        easter_purchase = Purchase(id=next(id_gen), city=city.name, item=next_item,
+                                                   price=next_price,
+                                                   influencer="easter")
+                        daily_purchases.append(easter_purchase.json())
+                        my_basket.purchases.append(easter_purchase)
+                        weighted_purchases -= 1
+                        easter_purchases += 1
+
+                    if random_gen.__next__() < my_score.valentines_weight:
+                        next_item, next_price = get_random_item_from_dict(my_groceries.valentines)
+                        valentines_purchase = Purchase(id=next(id_gen), city=city.name, item=next_item,
+                                                       price=next_price)
+
+                        daily_purchases.append(valentines_purchase.json())
+                        my_basket.purchases.append(valentines_purchase)
+                        weighted_purchases -= 1
+
+                    if random_gen.__next__() < my_score.new_year_weight:
+                        next_item, next_price = get_random_item_from_dict(my_groceries.new_year)
+                        new_year_purchase = Purchase(id=next(id_gen), city=city.name, item=next_item,
+                                                     price=next_price,
+                                                     influencer="new_year")
+                        daily_purchases.append(new_year_purchase.json())
+                        my_basket.purchases.append(new_year_purchase)
+                        weighted_purchases -= 1
+                        newyear_purchases += 1
+                        pass
+                    pass
+                await producer.send(topic_name, my_basket.json().encode("utf-8"))
+
+            total_purchases += len(daily_purchases)
+            json_purchases = json.dumps(daily_purchases)
+
+            # pprint(json_purchases)
 
 
-        # pprint(json_purchases)
+#
+# pprint(
+#     f"total purchases simulated is {total_purchases}, {newyear_purchases} new year, {easter_purchases} easter, {thanksgiving_purchases} thanksgiving, {xmas_purchases} xmas")
+#
 
-pprint(f"total purchases simulated is {total_purchases}, {newyear_purchases} new year, {easter_purchases} easter, {thanksgiving_purchases} thanksgiving, {xmas_purchases} xmas")
+
+async def main():
+    global producer
+    producer = aiokafka.AIOKafkaProducer(
+        bootstrap_servers=f"streaming-econ-demo-devrel-ben.aivencloud.com:15545",
+        security_protocol="SSL",
+        ssl_context=context,
+        # broker_credentials=context,
+    )
+
+    await producer.start()
+    await my_loop(producer)
+    await producer.stop()
+
+
+asyncio.run(main())
